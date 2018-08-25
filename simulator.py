816ec1c3c5f6a32abda6dd3fd612600a8f078858
James Elderfield <jad.elderfield@gmail.com>
2018-08-25 16:06:39 +0300

    Add debug mode. Blobs move around. Blobs do not leave screen.

--- simulator.py
+++ simulator.py
@@ -1,4 +1,6 @@
 from world_state import WorldState
+from random import random
+from blob import Blob
 
 
 class Simulator:
@@ -6,6 +8,20 @@ class Simulator:
         self.current_state = WorldState.generate_random_state()
 
     def get_next_state(self):
-        # TODO: calculate something
+        max_step_size = Blob.radius * 2
+        
+        for blob in self.current_state.blobs:
+            blob.x += random() * 2 * max_step_size - max_step_size
+            blob.y += random() * 2 * max_step_size - max_step_size
+
+            if blob.x + blob.radius > 1:
+                blob.x = 1 - blob.radius
+            elif blob.x - blob.radius < 0:
+                blob.x = blob.radius
+
+            if blob.y + blob.radius > 1:
+                blob.y = 1 - blob.radius
+            elif blob.y - blob.radius < 0:
+                blob.y = blob.radius
 
         return self.current_state
